cbe233c8c903ec10341cfd44b4917e7ed716e79c
Karin Skans <skka19st@student.ju.se>
2019-10-31 12:32:50 +0100

    fungerande version

---
+++ routes/land.js
@@ -0,0 +1,21 @@
+// 'require' importerar angiven modul(package)
+// 'express' är ett ramverk som underlättar start av server
+// 'fs' = FileSystem, dvs hantering av filer 
+var express = require('express');
+var fs = require('fs');
+var router = express.Router();
+
+// läs fil med länder
+// .get innebär läsning från filen
+router.get('/', function(req, res, next) {
+  fs.readFile('land.json', (err,data) => {
+    if (err) throw err;
+    var land = JSON.parse(data);  // gör om jsonfil till textarray
+
+    // .send visar innehållet på servern (webbläsaren)
+    // blir som ett API för andra program
+    res.send(land);       
+  })
+});
+
+module.exports = router;
